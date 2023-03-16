d63fabdbe848bc25c47b9725993f5c6ebf658e92
Mamedov98 <110538493+Mamedov98@users.noreply.github.com>
2023-03-16 12:54:13 +0400

    Add files via upload

---
+++ NetworkProgHw1/NetworkProgHw1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+
+{
+    static async Task Main(string[] args)
+    {
+
+
+        using (HttpClient client = new HttpClient())
+        {
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact");
+                if (response.IsSuccessStatusCode) 
+                {
+                  string  ResponseForApi = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(ResponseForApi);
+                }
+                else
+                { Console.WriteLine("Error",response.StatusCode); }
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+    }
+}
