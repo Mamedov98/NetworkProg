de0ab31d69486faee6a862bc1685744514f36082
Mamedov98 <110538493+Mamedov98@users.noreply.github.com>
2023-04-20 14:47:14 +0400

    Add files via upload

---
+++ FTPhw/FTPhw/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace FtpConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri("ftp://127.0.0.1/")) as FtpWebRequest;
+
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+            request.Credentials = new NetworkCredential("SECRETS", "12345");
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+
+
+            StreamReader reader = new StreamReader(responseStream);
+
+            Console.WriteLine(reader.ReadToEnd());
+            
+        }
+    }
+}
